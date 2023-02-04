d9a6e8589d1db735a9de1c955aafaf35beb1e019
lixiangchun <364592279@qq.com>
2023-02-04 19:19:22 +0800

    first commit

--- train.sh
+++ train.sh
@@ -3,10 +3,12 @@
 export OMP_NUM_THREADS=1
 export TOKENIZERS_PARALLELISM=false
 
-python train.py --train-file trn.txt --val-file val.txt \
--j 1 --max-len 64 -b 8 --epochs 10 --device cpu \
+# If no GPU available, set `--device cpu`
+
+python train.py --train-file data/Muris_gene_rankings.txt.gz --val-file data/val.txt \
+-j 1 --max-len 64 -b 8 --epochs 10 --device cuda \
 --lr 0.003 --lr_scheduler_type cosine \
 --output-dir checkpoint \
 --tokenizer_dir gpt2tokenizer \
---print-freq 2
+--print-freq 10
 
--- train_parallel.sh
+++ train_parallel.sh
@@ -4,12 +4,14 @@ export OMP_NUM_THREADS=1
 export TOKENIZERS_PARALLELISM=false
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
+# An example of training on NVIDIA A100
+
 torchrun --nproc_per_node=8 train.py
+--train-file data/Muris_gene_rankings.txt.gz --val-file data/val.txt \
 -j 1 --max-len 64 \
 --lr 0.003 --lr_scheduler_type cosine \
 --output-dir checkpoint \
 --tokenizer_dir gpt2tokenizer \
---print-freq 100
-
+--print-freq 10
 
 
